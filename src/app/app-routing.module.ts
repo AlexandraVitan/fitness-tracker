07117f235737b2f6540803eed41c3c654ce8cbdf
GruiaAnton <vitan.alexandra.i7i@student.ucv.ro>
2023-09-26 09:50:08 +0300

    added progress spinner at current trainings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {WelcomeComponent} from './welcome/welcome.component';
 import { SingupComponent } from './auth/singup/singup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 
 
 const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
   {path: 'signup', component: SingupComponent},
   {path:'login', component: LoginComponent},
-  {path:'training', component: TrainingComponent}
+  {path:'training', component: TrainingComponent},
+  {path:'current-training', component: CurrentTrainingComponent},
+  {path: 'new-training', component: NewTrainingComponent},
+  {path: 'past-trainings', component: PastTrainingsComponent},
 ];
 
 @NgModule({
--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-training',
@@ -6,12 +7,22 @@ import { Component, EventEmitter, Output, OnInit } from '@angular/core';
   styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
-  @Output() trainingStart = new EventEmitter<void>();
-  ongoingTraining = false;
-  constructor() {}
+ // @Output() trainingStart = new EventEmitter<void>();
+ // ongoingTraining = false;
+  //constructor() {}
+  selectedExercise: string | undefined;
+
+  constructor(private router: Router) {}
   ngOnInit(): void {}
 
-  onStartTraining() {
-    this.trainingStart.emit();
-  }
+  // onStartTraining() {
+  //   this.trainingStart.emit();
+  // }
+  
+
+  startTraining() {
+    // You can use the selectedExercise value if needed to pass data to the Current Training component
+    // For now, let's assume you just want to navigate to the Current Training component
+    this.router.navigate(['/current-training']);
+}
 }
