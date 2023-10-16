cbc08ff55c358e670f48ccb1e31a6c5dff03ca7e
GruiaAnton <vitan.alexandra.i7i@student.ucv.ro>
2023-10-16 10:46:37 +0300

    adding data table(past trainings)

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -15,6 +15,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   imports: [
@@ -33,7 +34,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatSelectModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule
     
   ],
   exports: [
@@ -53,6 +55,7 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatProgressBarModule,
     MatProgressSpinnerModule,
     MatDialogModule,
+    MatTableModule
   ],
 })
 export class MaterialModule {}
--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { TrainingService } from './../training.service';
+import { Exercise } from './../exercise.model';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
-  styleUrls: ['./past-trainings.component.css']
+  styleUrls: ['./past-trainings.component.css'],
 })
-export class PastTrainingsComponent {
+export class PastTrainingsComponent implements OnInit {
+  displayedColumns = ['date','name','duration','calories','state'];
+  dataSource = new MatTableDataSource <Exercise>();
 
+
+  constructor(private trainingService: TrainingService) {
+
+  }
+  ngOnInit(): void {
+    this.dataSource.data= this.trainingService.getCompletedOrCancelledExercises();
+  }
 }
