b6a2af6ffb61acbbaeca0b46b010f020c4a71c35
GruiaAnton <vitan.alexandra.i7i@student.ucv.ro>
2023-11-18 20:33:19 +0200

    moving the auth guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,9 +1,10 @@
-import { CanActivate} from '@angular/router';
+import { CanActivate, Route } from '@angular/router';
 import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router,
+  CanLoad,
 } from '@angular/router';
 
 import { AuthService } from './auth.service';
@@ -11,7 +12,19 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+  canLoad(route: Route) {
     if (this.authService.isAuth()) {
       return true;
     } else {
