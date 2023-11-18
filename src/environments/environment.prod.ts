d725dc125aad730907747ed2c87093dd94953317
GruiaAnton <vitan.alexandra.i7i@student.ucv.ro>
2023-11-18 20:49:28 +0200

    modify something for prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+//import * as firebase from "firebase";
+import { initializeApp } from 'firebase/app';
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBlaQkoka513UexxgawSwhQeqvDa3fTZ24',
+    authDomain: 'ng-angular-project-bd17e.firebaseapp.com',
+    projectId: 'ng-angular-project-bd17e',
+    storageBucket: 'ng-angular-project-bd17e.appspot.com',
+    messagingSenderId: '687979851346',
+    appId: '1:687979851346:web:037406da09f9055507798b',
+    measurementId: 'G-BNJXJF3DGD',
+  },
+};
+  //export const app = initializeApp(environment.firebase);
