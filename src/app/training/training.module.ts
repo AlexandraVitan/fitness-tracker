2aced238def4391e030fcba34064ef102fbce444
GruiaAnton <vitan.alexandra.i7i@student.ucv.ro>
2023-11-18 17:51:51 +0200

    added Training Module

---
+++ src/app/training/training.module.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TrainingComponent } from './training.component';
+import { CurrentTrainingComponent } from './current-training/current-training.component';
+import { NewTrainingComponent } from './new-training/new-training.component';
+import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
+import { StopTrainingComponent } from './current-training/stop-training.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+
+@NgModule({
+  declarations: [
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    StopTrainingComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AngularFirestoreModule,
+  ],
+  exports: []
+})
+export class TrainingModule {}
